f31d35a718000a06318cc8d54c52e1950f3bf8a4
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-08 22:07:51 -0300

    feat: link on navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { deleteCookie } from "@/actions/delete-cookie";
 import { useTheme } from "next-themes";
 import { Landmark, Laptop2Icon, Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 import {
   DropdownMenu,
@@ -18,10 +20,7 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { useRouter } from "next/dist/client/router";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import Link from "next/link";
 
 const teams: ItensProps[] = [
   {
@@ -57,36 +56,33 @@ export default function Navbar({ name, email }: NavbarProps) {
 
   return (
     <div className="w-full py-3 px-5 border-b border-muted flex flex-row justify-between items-center">
-      <div className="space-x-3 flex items-center">
+      <div className="flex items-center gap-8">
         {/* <Combobox data={teams} /> */}
         <h1 className="pr-10 flex items-center gap-3">
           <Landmark strokeWidth={2} className="text-primary w-5" />
           Rugby Finance
         </h1>
-        <Button
-          variant={"ghost"}
-          className={
-            pathName === "/rugby-payment" ? undefined : "text-muted-foreground"
-          }
+        <Link
+          href={"/rugby-payment"}
+          className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground 
+            ${pathName === "/rugby-payment" ? undefined : "text-muted-foreground"}`}
         >
           Mensalidades
-        </Button>
-        <Button
-          variant={"ghost"}
-          className={
-            pathName === "/members" ? undefined : "text-muted-foreground"
-          }
+        </Link>
+        <Link
+          href={"/members"}
+          className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground 
+            ${pathName === "/members" ? undefined : "text-muted-foreground"}`}
         >
           Membros
-        </Button>
-        <Button
-          variant={"ghost"}
-          className={
-            pathName === "/payments" ? undefined : "text-muted-foreground"
-          }
+        </Link>
+        <Link
+          href={"/payments"}
+          className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground 
+            ${pathName === "/payments" ? undefined : "text-muted-foreground"}`}
         >
           Pagamentos
-        </Button>
+        </Link>
       </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>

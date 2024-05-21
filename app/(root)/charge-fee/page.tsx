067dab4d6019bbc62f0cee233cf7dddceb84c502
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-20 23:13:46 -0300

    feat: charge fee page

--- app/(root)/charge-fee/page.tsx
+++ app/(root)/charge-fee/page.tsx
@@ -1,27 +1,28 @@
 "use client";
 
 import { ReactNode, useEffect, useState } from "react";
-import { Check } from "lucide-react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import EmailStep from "./components/email-step";
 import WhatsappStep from "./components/whatsapp-step";
+import { StepCircle } from "./components/progress-circle";
+import FinalStep from "./components/final-step";
 
 export default function ChargeFee() {
   const [stage, setStage] = useState<"first" | "second" | "end">("first");
   const [stageContent, setStageContent] = useState<ReactNode>();
 
+  const moveToFinalStep = () => {
+    setStageContent(<FinalStep />);
+    setStage("end");
+  };
+
   const moveToSecondStep = () => {
-    setStageContent(<WhatsappStep />);
+    setStageContent(<WhatsappStep nextStage={moveToFinalStep} />);
     setStage("second");
   };
 
   useEffect(() => {
     setStageContent(<EmailStep nextStage={moveToSecondStep} />);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -45,40 +46,5 @@ export default function ChargeFee() {
       </div>
       {stageContent}
     </div>
-  );
-}
-
-interface StepCircleProps {
-  stepStage?: number;
-  tooptipText: string;
-  selected?: boolean;
-}
-
-function StepCircle({
-  stepStage,
-  tooptipText,
-  selected = false,
-}: StepCircleProps) {
-  let triggerContent: string | ReactNode = <Check className="h-4 w-4" />;
-
-  if (stepStage) {
-    triggerContent = stepStage;
-  }
-
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div
-            className={`h-7 w-7 bg-background ${selected ? "bg-primary text-primary-foreground" : "text-muted-foreground border-muted-foreground"} rounded-full flex items-center justify-center text-sm border z-10 cursor-pointer`}
-          >
-            {triggerContent}
-          </div>
-        </TooltipTrigger>
-        <TooltipContent side="bottom" sideOffset={5}>
-          <p className="text-sm">{tooptipText}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
   );
 }

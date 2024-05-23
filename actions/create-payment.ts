8b584e527ef186fdb48ba971d4cf97a10f8dc8d5
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-23 13:30:32 -0300

    feat: create payment route update

--- actions/create-payment.ts
+++ actions/create-payment.ts
@@ -1,29 +1,39 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { Member } from "@prisma/client";
 
 interface createPaymentProps {
   memberId: string;
   memberName: string;
   value: number;
-  type: "pix" | "cash";
+  type?: "pix" | "cash";
   cause: string;
+  member?: Member;
 }
 
 export const createPayment = async ({
   memberId,
   memberName,
   value,
-  type,
+  type = "pix",
   cause,
 }: createPaymentProps) => {
-  await prisma.payment.create({
-    data: {
-      memberId: memberId,
-      memberName: memberName,
-      value: value,
-      type: type,
-      cause: cause,
-    },
-  });
+  try {
+    const response = await prisma.payment.create({
+      data: {
+        memberId,
+        memberName,
+        value,
+        type,
+        cause,
+      },
+    });
+
+    if (!response) {
+      throw new Error("Error creating Payment!");
+    }
+  } catch (error) {
+    console.log("Error: " + error);
+  }
 };

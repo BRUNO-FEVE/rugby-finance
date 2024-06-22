8f1fa0dc14f45f6f73e3fbbe838d035923a40f51
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-22 16:32:52 -0300

    ui: tabs component

--- prisma/seed-member.ts
+++ prisma/seed-member.ts
@@ -1,44 +1,44 @@
-// import { prisma } from "@/lib/prisma";
-// import { memberJson } from "./seed-data/member-data";
+import { prisma } from "@/lib/prisma";
+import { memberJson } from "./seed-data/member-data";
 
-// async function main() {
-//   await prisma.member.deleteMany();
+async function main() {
+  await prisma.member.deleteMany();
 
-//   const CURRENT_YEAR = new Date().getFullYear();
+  const CURRENT_YEAR = new Date().getFullYear();
 
-//   memberJson.map(async (member) => {
-//     await prisma.member.create({
-//       data: {
-//         email: member.email,
-//         name: member.name,
-//         nickname: member.nickName,
-//         cpf: BigInt(member.cpf),
-//         rg: member.rg,
-//         educationInstituition: member.educationInstituition,
-//         ra: member.ra,
-//         course: member.course,
-//         yearOfGraduation: member.yearOfGraduation,
-//         phoneNumber: BigInt(member.phoneNumber),
-//         dateOfBirth: member.yearOfBirth,
-//         yearOfJoinOnRugbyMaua: member.yearOfJoinOnRugbyMaua,
-//         instagram: member.instagram,
-//         lawsLink: member.lawsLink,
-//         readyLink: member.readyLink,
-//         team: member.team,
-//         uniformSize: member.uniformSize,
-//         uniformNumber: member.uniformNumber ?? null,
-//         isPaying: member.yearOfGraduation >= CURRENT_YEAR,
-//       },
-//     });
-//   });
-// }
+  memberJson.map(async (member) => {
+    await prisma.member.create({
+      data: {
+        email: member.email,
+        name: member.name,
+        nickname: member.nickName,
+        cpf: BigInt(member.cpf),
+        rg: member.rg,
+        educationInstituition: member.educationInstituition,
+        ra: member.ra,
+        course: member.course,
+        yearOfGraduation: member.yearOfGraduation,
+        phoneNumber: BigInt(member.phoneNumber),
+        dateOfBirth: member.yearOfBirth,
+        yearOfJoinOnRugbyMaua: member.yearOfJoinOnRugbyMaua,
+        instagram: member.instagram,
+        lawsLink: member.lawsLink,
+        readyLink: member.readyLink,
+        team: member.team,
+        uniformSize: member.uniformSize,
+        uniformNumber: member.uniformNumber ?? null,
+        isPaying: member.yearOfGraduation >= CURRENT_YEAR,
+      },
+    });
+  });
+}
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

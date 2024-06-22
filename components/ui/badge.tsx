0d7684421494448bdede172a149941564f7487b5
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-22 19:08:50 -0300

    ui: badge component

---
+++ components/ui/badge.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+        double: "w-fit h-7 p-0 pl-2 pr-1 flex flex-row gap-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {
+  title?: string;
+  content?: string;
+}
+
+function Badge({ className, variant, title, content, ...props }: BadgeProps) {
+  if (variant === "double") {
+    return (
+      <Badge
+        variant={"secondary"}
+        className={cn(badgeVariants({ variant }), className)}
+        {...props}
+      >
+        {title}
+        <Badge variant={"default"}>{content}</Badge>
+      </Badge>
+    );
+  }
+
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };

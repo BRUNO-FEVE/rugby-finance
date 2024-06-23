c426e37966f83ca83346750e019e7aa0d3c8ca09
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-23 20:23:13 -0300

    refat: add payment drawer

--- app/(root)/rugby-payment/components/add-payment-drawer.tsx
+++ app/(root)/rugby-payment/components/add-payment-drawer.tsx
@@ -30,7 +30,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import ManualAddPaymentForm from "./manual-add-payment-form";
 import AutoAddPaymentForm from "./auto-add-payment-form";
-import AutoPaymentInfoDialog from "./auto-payment-info-dialog";
+import AutoPaymentInfoDialog from "./payment-modes-info-dialog";
 
 interface AddPaymentDrawerProps {
   member: Member;
@@ -59,93 +59,99 @@ export default function AddPaymentDrawer({ member }: AddPaymentDrawerProps) {
             <ClipboardEditIcon className="w-4 h-4 " />
           </Button>
         </DrawerTrigger>
-        <DrawerContent className="mx-auto h-fit w-full flex flex-row gap-6 p-16 justify-between items-start">
-          <div className="w-2/5 flex flex-col gap-8">
-            <Card>
-              <CardHeader className=" flex flex-col gap-6">
-                <div className="flex flex-row items-center gap-3">
-                  <Avatar className="w-20 h-20">
-                    <AvatarImage></AvatarImage>
-                    <AvatarFallback className="text-xs">
-                      {getNameInitials(member.name)}
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col gap-2">
-                    <CardTitle>
-                      {member.name}{" "}
-                      <span className="text-muted-foreground">
-                        ({member.nickname})
-                      </span>
-                    </CardTitle>
-                    <div className="flex flex-row gap-2">
-                      <Tag size="sm" label={member.team} />
-                      <Tag size="sm" label={member.yearOfJoinOnRugbyMaua} />
+        <DrawerContent className="h-fit">
+          <div className="mx-auto w-full flex flex-row gap-6 p-16 justify-between items-center">
+            <div className="w-2/5 flex flex-col gap-8">
+              <Card>
+                <CardHeader className=" flex flex-col gap-6">
+                  <div className="flex flex-row items-center gap-3">
+                    <Avatar className="w-20 h-20">
+                      <AvatarImage></AvatarImage>
+                      <AvatarFallback className="text-xs">
+                        {getNameInitials(member.name)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col gap-2">
+                      <CardTitle>
+                        {member.name}{" "}
+                        <span className="text-muted-foreground">
+                          ({member.nickname})
+                        </span>
+                      </CardTitle>
+                      <div className="flex flex-row gap-2">
+                        <Tag size="sm" label={member.team} />
+                        <Tag size="sm" label={member.yearOfJoinOnRugbyMaua} />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </CardHeader>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>Valores das mensalidades</CardTitle>
-                <CardDescription>
-                  Lembre-se de que esses valores podem variar, dependendo de
-                  promoções e atualizações ao longo dos anos.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="flex flex-row text-sm text-muted-foreground gap-7">
-                <div className="flex flex-col items-start">
-                  <p>Anual</p>
-                  <span className="font-bold text-4xl text-primary">$250</span>
-                </div>
-                <div className="flex flex-col items-start">
-                  <p>Semestre</p>
-                  <span className="font-bold text-4xl text-primary">$130</span>
-                </div>
-                <div className="flex flex-col items-start">
-                  <p>Mensal</p>
-                  <span className="font-bold text-4xl text-primary">$30</span>
-                </div>
-              </CardContent>
-              <CardFooter className="pb-5 px-5">
-                <div className="flex flex-row items-center rounded-md border p-3 gap-3">
-                  <Coins className="h-10 w-10 text-muted-foreground" />
+                </CardHeader>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Valores das mensalidades</CardTitle>
                   <CardDescription>
-                    Valores atualizados pela ultima vez no dia 12/05/24
+                    Lembre-se de que esses valores podem variar, dependendo de
+                    promoções e atualizações ao longo dos anos.
                   </CardDescription>
-                </div>
-              </CardFooter>
-            </Card>
-          </div>
-          <div className="w-full h-full flex flex-col items-start justify-start gap-4">
-            <DrawerHeader>
-              <DrawerTitle className="pb-0 h-fit">
-                Adicionar Pagamento de Mesalidade
-              </DrawerTitle>
-              <DrawerDescription>
-                Esta é a seção onde você pode adicionar um novo pagamento de
-                mensalidade. Preencha os campos necessários com as informações
-                corretas e clique em <strong>Salvar</strong> para confirmar o
-                pagamento.
-              </DrawerDescription>
-            </DrawerHeader>
-            <Separator />
-            <Tabs defaultValue={"auto"} className="h-full w-full py-6 px-8">
-              <TabsList className="relative">
-                <TabsTrigger value="auto">Automatico</TabsTrigger>
-                <TabsTrigger value="manual">Manual</TabsTrigger>
-                <AutoPaymentInfoDialog />
-              </TabsList>
-              <TabsContent value="auto" className="h-full">
-                <AutoAddPaymentForm member={member} />
-              </TabsContent>
-              <TabsContent value="manual" className="h-full">
-                <ManualAddPaymentForm
-                  member={member}
-                  closeDrawer={closeDrawer}
-                />
-              </TabsContent>
-            </Tabs>
+                </CardHeader>
+                <CardContent className="flex flex-row text-sm text-muted-foreground gap-7">
+                  <div className="flex flex-col items-start">
+                    <p>Anual</p>
+                    <span className="font-bold text-4xl text-primary">
+                      $250
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <p>Semestre</p>
+                    <span className="font-bold text-4xl text-primary">
+                      $130
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <p>Mensal</p>
+                    <span className="font-bold text-4xl text-primary">$30</span>
+                  </div>
+                </CardContent>
+                <CardFooter className="pb-5 px-5">
+                  <div className="flex flex-row items-center rounded-md border p-3 gap-3">
+                    <Coins className="h-10 w-10 text-muted-foreground" />
+                    <CardDescription>
+                      Valores atualizados pela ultima vez no dia 12/05/24
+                    </CardDescription>
+                  </div>
+                </CardFooter>
+              </Card>
+            </div>
+            <div className="w-full h-full flex flex-col items-start justify-start gap-4">
+              <DrawerHeader>
+                <DrawerTitle className="pb-0 h-fit">
+                  Adicionar Pagamento de Mesalidade
+                </DrawerTitle>
+                <DrawerDescription>
+                  Esta é a seção onde você pode adicionar um novo pagamento de
+                  mensalidade. Preencha os campos necessários com as informações
+                  corretas e clique em <strong>Salvar</strong> para confirmar o
+                  pagamento.
+                </DrawerDescription>
+              </DrawerHeader>
+              <Separator />
+              <Tabs defaultValue={"auto"} className="h-full w-full py-6 px-8">
+                <TabsList className="relative">
+                  <TabsTrigger value="auto">Automatico</TabsTrigger>
+                  <TabsTrigger value="manual">Manual</TabsTrigger>
+                  <AutoPaymentInfoDialog />
+                </TabsList>
+                <TabsContent value="auto" className="h-full">
+                  <AutoAddPaymentForm member={member} />
+                </TabsContent>
+                <TabsContent value="manual" className="h-full">
+                  <ManualAddPaymentForm
+                    member={member}
+                    closeDrawer={closeDrawer}
+                  />
+                </TabsContent>
+              </Tabs>
+            </div>
           </div>
         </DrawerContent>
       </Drawer>

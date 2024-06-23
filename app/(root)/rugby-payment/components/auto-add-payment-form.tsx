09158358ebd53d9bac240692f01d600404763bec
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-23 20:23:40 -0300

    feat: logic on auto payment layout

--- app/(root)/rugby-payment/components/auto-add-payment-form.tsx
+++ app/(root)/rugby-payment/components/auto-add-payment-form.tsx
@@ -10,12 +10,25 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Member } from "@prisma/client";
-import { getAbbreviatedMonth } from "@/lib/utils";
+import {
+  getAbbreviatedMonth,
+  getIndexMonthsToPay,
+  getMonthsToPay,
+} from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 interface AutoAddPaymentFormProps {
   member: Member;
@@ -29,6 +42,10 @@ const autoAddPaymentFormSchema = z.object({
 export default function AutoAddPaymentForm({
   member,
 }: AutoAddPaymentFormProps) {
+  const [futureMonthsToPay, setFutureMonthsToPay] = useState<number[]>([]);
+  const [monthsToPayInOrder, setMonthsToPayInOrder] = useState<number[]>([]);
+  const MONTHS_TO_PAY = getMonthsToPay(member.paymentRecord).split(" ");
+
   const form = useForm<z.infer<typeof autoAddPaymentFormSchema>>({
     resolver: zodResolver(autoAddPaymentFormSchema),
     defaultValues: {
@@ -40,6 +57,10 @@ export default function AutoAddPaymentForm({
   function onSubmit(values: z.infer<typeof autoAddPaymentFormSchema>) {
     console.log(values);
   }
+
+  useEffect(() => {
+    setMonthsToPayInOrder(getIndexMonthsToPay(member.paymentRecord));
+  }, []);
 
   return (
     <div className="w-full h-full flex flex-col gap-7">
@@ -55,6 +76,7 @@ export default function AutoAddPaymentForm({
                 variant={"double"}
                 title={getAbbreviatedMonth(index)}
                 content={`R$ ${month}`}
+                className={`${month > 0 ? "bg-green-500 hover:bg-green-600" : null} ${futureMonthsToPay.includes(index) ? "bg-blue-700" : null}`}
               />
             );
           })}
@@ -79,6 +101,7 @@ export default function AutoAddPaymentForm({
                       {...field}
                     />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               );
             }}
@@ -90,13 +113,37 @@ export default function AutoAddPaymentForm({
               return (
                 <FormItem className="w-full">
                   <FormLabel>Meses</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="w-full"
-                      placeholder="Insira o meses desse pagamento"
-                      {...field}
-                    />
-                  </FormControl>
+                  <Select
+                    onValueChange={(event) => {
+                      field.onChange(event);
+                      setFutureMonthsToPay(
+                        monthsToPayInOrder.slice(
+                          0,
+                          form.getValues().monthsPaid,
+                        ),
+                      );
+                    }}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione os meses..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {MONTHS_TO_PAY.map((month, index) => {
+                        const monthQuantite = index + 1;
+                        return (
+                          <SelectItem
+                            key={month + index}
+                            value={monthQuantite.toString()}
+                          >
+                            {monthQuantite}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
                 </FormItem>
               );
             }}
